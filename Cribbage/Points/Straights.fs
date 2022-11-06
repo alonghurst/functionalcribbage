72a7449e836fefc65b65178c88c5b39623f1810d
Andy Longhurst <andy@longhurst.net>
2022-11-06 18:55:41 +0000

    adjacency functions

--- Cribbage/Points/Straights.fs
+++ Cribbage/Points/Straights.fs
@@ -7,6 +7,33 @@ module Straights =
         |> Array.append (Combinations.Generate cards 4)
         |> Array.append (Combinations.Generate cards 5)
 
+    let IsAdjacent cardA cardB = 
+        let (a, b) = 
+            match cardA with 
+            | _ when cardA.Face < cardB.Face -> (cardA.Face, cardB.Face)
+            | _ -> (cardB.Face, cardA.Face)
+        match a with
+            | Ace when b = Two -> true
+            | Two when b = Three -> true
+            | Three when b = Four -> true
+            | Four when b = Five -> true
+            | Five when b = Six -> true
+            | Six when b = Seven -> true
+            | Seven when b = Eight -> true
+            | Eight when b = Nine -> true
+            | Nine when b = Ten -> true
+            | Ten when b = Jack -> true
+            | Jack when b = Queen -> true
+            | Queen when b = King -> true
+            | _ -> false
+
+    let rec AreAllAdjacent cards =
+        let sortedCards = cards |> Cards.Sort 
+        match sortedCards with
+        | _ when sortedCards.Length <= 1 -> true
+        | _ when IsAdjacent sortedCards[0] sortedCards[1] -> AreAllAdjacent sortedCards[1..]
+        | _ -> false
+
     let IsStraight cards = false
 
     let Get cards =
