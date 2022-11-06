bcef50f873a2ba9531cf8b2bf0f8f3fbc25be94b
Andy Longhurst <andy@longhurst.net>
2022-11-06 12:08:21 +0000

    another test

--- Cribbage.Tests/Cards.fs
+++ Cribbage.Tests/Cards.fs
@@ -9,3 +9,8 @@ module ParseCard =
     let ``King of Hearts`` () =
         let card = Cards.CardFromString "hk"
         AssertCard card Hearts King
+
+    [<Fact>]
+    let ``Ten of Diamonds`` () =
+        let card = Cards.CardFromString "d10"
+        AssertCard card Diamonds Ten
